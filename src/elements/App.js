8e1f53a82263158de88a742618120a1320e42729
Martin Gonzalez <martin.hgonzalez21@gmail.com>
2020-02-10 04:12:14 -0300

    3 pendings from Joplin

---
+++ src/elements/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>OK</p>
+        <h1>Hello World</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
